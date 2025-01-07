2a4443cbd1d70f54fc3850649c6c239dc4fb0141
Renato28 <renatonobrega87@gmail.com>
2025-01-06 21:02:45 -0300

    projeto criado

---
+++ src/main/java/io/github/clean_arquiteture/CleanArquitetureApplication.java
@@ -0,0 +1,13 @@
+package io.github.clean_arquiteture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArquitetureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArquitetureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/clean_arquiteture/CleanArquitetureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.clean_arquiteture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArquitetureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
